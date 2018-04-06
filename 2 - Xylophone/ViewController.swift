eb1f06096aa66ad5e4cf2dc3cbd6344dfaa88b2d
Kelvin Tan <kelvintan@Kelvins-MacBook-Pro.local>
2018-04-06 14:39:41 +0800

    First Commit

--- 2 - Xylophone/ViewController.swift
+++ 2 - Xylophone/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    var player: AVAudioPlayer!
+    var selected: String = ""
+    let sounds = ["note1","note2","note3","note4","note5","note6","note7"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func notePressed(_ sender: UIButton) {
+        selected = sounds[sender.tag]
+        playSound()
     }
-
-
+    
+    func playSound(){
+        let url = Bundle.main.url(forResource: selected, withExtension: "wav")
+        do {
+            player = try AVAudioPlayer(contentsOf: url!)
+        } catch {
+            print(error)
+        }
+        player.play()
+    }
+    
 }
 
